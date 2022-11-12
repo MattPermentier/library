5ab9b9fe505d00213df7829a384679976aa399c6
mattp <matt.permentier@gmail.com>
2022-11-12 16:22:46 +0100

    books are added using a class

--- script.js
+++ script.js
@@ -10,33 +10,41 @@ let submitBtn = document.getElementById('submitBtn');
 let allBooks = document.getElementById('allBooks');
 let books = document.getElementById('showBooks');
 
-let readButton;
 // event listeners
-submitBtn.addEventListener('click', addBookToLibrary);
 addBook.addEventListener('click', openForm);
 body.addEventListener('click', closeForm);
+
+// global variables
+let readButton;
+let removeButton;
+let book;
 
 // array to save all books in
 let myLibrary = [];
 
-// constructor function
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-// add a new book to the library
-function addBookToLibrary(e) {
-  e.preventDefault();
-  let book = new Book(title.value, author.value, pages.value, isChecked());
-  myLibrary.push(book);
-  cleanInput();
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title.value;
+    this.author = author.value;
+    this.pages = pages.value;
+    this.read = read.value;
+  }
 
+  addToLibrary() {
+    myLibrary.push([title.value, author.value, pages.value, isChecked()]);
+    console.log(myLibrary);
+  }
+}
+book = new Book(title.value, author.value, pages.value, isChecked());
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  book.addToLibrary();
+  cleanInput();
   allBooks.innerHTML = '';
   showBooks();
-}
-let removeButton;
+  console.log(myLibrary[0][3]);
+});
 
 // loop throught the array with all books
 function showBooks() {
@@ -52,17 +60,17 @@ function saveBooks(i) {
   // title of book
   let bookTitle = document.createElement('p');
   bookTitle.classList.add('bookInfo');
-  bookTitle.innerHTML = `"${myLibrary[i].title}"`;
+  bookTitle.innerHTML = `"${myLibrary[i][0]}"`;
   libraryContainer.appendChild(bookTitle);
   // author of book
   let bookAuthor = document.createElement('p');
   bookAuthor.classList.add('bookInfo');
-  bookAuthor.innerHTML = myLibrary[i].author;
+  bookAuthor.innerHTML = myLibrary[i][1];
   libraryContainer.appendChild(bookAuthor);
   // amount of pages
   let bookPages = document.createElement('p');
   bookPages.classList.add('bookInfo');
-  bookPages.innerHTML = `${myLibrary[i].pages} pages`;
+  bookPages.innerHTML = `${myLibrary[i][2]} pages`;
   libraryContainer.appendChild(bookPages);
   // read button
   readButton = document.createElement('button');
@@ -92,11 +100,11 @@ function isChecked() {
 
 // decide wheter read or not using checkbox value
 function readStatus(num) {
-  if (myLibrary[num].read === true) {
+  if (myLibrary[num][3] === true) {
     readButton.classList.remove('notRead');
     readButton.classList.add('read');
     readButton.innerHTML = 'Read';
-  } else if (myLibrary[num].read === false) {
+  } else if (myLibrary[num][3] === false) {
     readButton.classList.remove('read');
     readButton.classList.add('notRead');
     readButton.innerHTML = 'Not read';
